2d9f730d325b282ae5d685d7fbe4392290f596db
yuu <22yuu@naver.com>
2022-03-13 19:54:37 +0900

    JS home contact btn

--- main.js
+++ main.js
@@ -23,7 +23,16 @@ navbarMenu.addEventListener('click', (e) => {
     if(link === null) {
         return;
     }
-    console.log(link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth'});
+    scrollIntoView(link);
 })
+
+// Handle click on "contact me" button on home
+const homeContacttBtn = document.querySelector('.home__contact');
+homeContacttBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selectior) {
+    const scrollTo = document.querySelector(selectior);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+}
